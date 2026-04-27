4b63ebbe9f46497ab2ffd22695852e613ff02973
ryderX86 <ryder357@icloud.com>
2026-04-27 02:33:55 -0500

    useless note

--- minecraftlauncher/functions/text.py
+++ minecraftlauncher/functions/text.py
@@ -8,18 +8,18 @@ import json
 def indent(text: str | dict | list, amount: int = 2):
     """Indents all lines of a `str` with `amount:int` (default: `2`) spaces."""
 
-    if isinstance(text, dict):
-        text = json.dumps(text, indent=amount)
-    elif isinstance(text, list):
-        text = ", ".join(text)
-    elif isinstance(text, str):
-        pass
-    else:
-        raise TypeError(type(text).__name__)
+    match text:
+        case dict():
+            text = json.dumps(text, indent=amount)
+        case list():
+            text = ", ".join(text)
+        case str():
+            pass
+        case _:
+            raise TypeError(type(text).__name__)
 
     lines = text.split("\n")
 
-    # doing it this way so replacing it is permanent
     for i, line in enumerate(lines):
         line = (" " * amount) + line
         lines[i] = line  # type: ignore
